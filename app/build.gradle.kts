e3e3e98adafd1e61eba212dce83ae11ff7a42658
rsps1008 <rsps1008@gmail.com>
2026-08-30 13:38:29 +0800

    更新版本1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rsps1008.fxxklocation"
         minSdk = 31
         targetSdk = 37
-        versionCode = 18
-        versionName = "1.8"
+        versionCode = 19
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
